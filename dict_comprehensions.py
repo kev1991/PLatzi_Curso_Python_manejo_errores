22440bf4dd44e348faaaa23dd5d81256c9482271
Kebin J Martinez T <kebin.martinez.t@gmail.com>
2021-11-17 10:40:46 -0500

    dict_comprehensions.py

---
+++ dict_comprehensions.py
@@ -0,0 +1,11 @@
+def run():
+    my_dict = {}
+
+    for k in range (1, 101):
+        my_dict [k] = k**3
+        
+    print(my_dict)
+
+
+    if __name__ == '__main__':
+        run()
--- list_comprehensions.py
+++ list_comprehensions.py
@@ -1,15 +1,16 @@
 def kev():
-    # squares = []
-    # for k in range (1,101):
-    #     if k % 3 != 1:
-    #         squares.append(k**2)
-    # print(squares)
+    squares = []
+    for k in range (1,101):
+        if k % 3 != 1:
+            squares.append(k**2)
+    print(squares)
 
     squares = [k for k in range(1,100000) if k % 36 == 0]
     print(squares)
 
     squares = [k for k in range(1,100000) if k % 4 == 0 and k % 6 == 0 and k % 9 == 0]
     print(squares)
+
 
 if __name__ == "__main__":
     kev()
